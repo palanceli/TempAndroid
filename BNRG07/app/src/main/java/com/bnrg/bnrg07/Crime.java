b3d8d540e81edfd46a50c5c79be028b1cf988858
palance <palanceli@163.com>
2017-09-14 23:34:27 +0800

    commit changes

---
+++ BNRG07/app/src/main/java/com/bnrg/bnrg07/Crime.java
@@ -0,0 +1,50 @@
+package com.bnrg.bnrg07;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by palance on 2017/9/14.
+ */
+
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
