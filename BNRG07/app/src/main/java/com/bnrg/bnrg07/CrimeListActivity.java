fd204b6252fbbcd35fe5892e45d3f38c969c5ab0
palance <palanceli@163.com>
2017-10-21 14:03:26 +0800

    commit changes

--- BNRG07/app/src/main/java/com/bnrg/bnrg07/CrimeListActivity.java
+++ BNRG07/app/src/main/java/com/bnrg/bnrg07/CrimeListActivity.java
@@ -6,9 +6,9 @@ import android.support.v4.app.Fragment;
  * Created by palance on 2017/10/21.
  */
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment(){
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
---
+++ BNRG07/app/src/main/java/com/bnrg/bnrg07/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bnrg.bnrg07;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by palance on 2017/10/21.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
