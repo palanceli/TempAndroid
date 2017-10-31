af072fd7d9630d0f95088f66a0fee61a8c836952
palance <palanceli@163.com>
2017-10-31 15:31:03 +0800

    commit changes

--- BNRG07/app/src/main/java/com/bnrg/bnrg07/CrimeFragment.java
+++ BNRG07/app/src/main/java/com/bnrg/bnrg07/CrimeFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -156,6 +157,7 @@ public class CrimeFragment extends Fragment {
 
         mPhotoView = (ImageView)v.findViewById(R.id.crime_photo);
 
+        updatePhotoView();
         return v;
     }
 
@@ -197,6 +199,12 @@ public class CrimeFragment extends Fragment {
             }finally{
                 cursor.close();
             }
+        }else if(requestCode == REQUEST_PHOTO){
+            Uri uri = FileProvider.getUriForFile(getActivity(),
+                    "com.bnrg.bnrg07.criminalintent.fileprovider",
+                    mPhotoFile);
+            getActivity().revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            updatePhotoView();
         }
     }
 
@@ -228,5 +236,14 @@ public class CrimeFragment extends Fragment {
         }
         String report = getString(R.string.crime_report, mCrime.getTitle(), dateString, solvedString, suspect);
         return report;
+    }
+
+    private void updatePhotoView(){
+        if(mPhotoFile == null || !mPhotoFile.exists()){
+            mPhotoView.setImageDrawable(null);
+        }else{
+            Bitmap bitmap = PictureUtils.getScaledBitmap(mPhotoFile.getPath(), getActivity());
+            mPhotoView.setImageBitmap(bitmap);
+        }
     }
 }
