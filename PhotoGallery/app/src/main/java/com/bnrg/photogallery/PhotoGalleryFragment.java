413c1a0cfe904c178f45723ab88e06c80d392fb0
palance <palanceli@163.com>
2017-11-17 00:22:08 +0800

    commit changes

--- PhotoGallery/app/src/main/java/com/bnrg/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bnrg/photogallery/PhotoGalleryFragment.java
@@ -13,10 +13,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v7.widget.SearchView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,13 +34,18 @@ public class PhotoGalleryFragment extends Fragment {
     private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
 
     private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>>{
+        private String mQuery;
+
+        public FetchItemsTask(String query){
+            mQuery = query;
+        }
+
         @Override
         protected List<GalleryItem> doInBackground(Void ... params){
-            String query = "robot";
-            if(query == null){
+            if(mQuery == null){
                 return new FlickrFetchr().fetchRecentPhotos();
             }else{
-                return new FlickrFetchr().searchPhotos(query);
+                return new FlickrFetchr().searchPhotos(mQuery);
             }
         }
 
@@ -59,7 +65,7 @@ public class PhotoGalleryFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
         setHasOptionsMenu(true);
-        new FetchItemsTask().execute();
+        updateItems();
         Handler responseHandler = new Handler();
         mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
         mThumbnailDownloader.setThumbnailDownloaderListener(
@@ -80,6 +86,50 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater){
         super.onCreateOptionsMenu(menu, menuInflater);
         menuInflater.inflate(R.menu.fragment_photo_gallery, menu);
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        final SearchView searchView = (SearchView)searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(
+                new SearchView.OnQueryTextListener(){
+                    @Override
+                    public boolean onQueryTextSubmit(String s){
+                        Log.d(TAG, "QueryTextSubmit: " + s);
+                        QueryPreferences.setStoredQuery(getActivity(), s);
+                        updateItems();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String s){
+                        Log.d(TAG, "QueryTextChange: " + s);
+                        return false;
+                    }
+                });
+        searchView.setOnSearchClickListener(new
+        View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String query = QueryPreferences.getStoredQuery(getActivity());
+                searchView.setQuery(query, false);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.menu_item_clear:
+                QueryPreferences.setStoredQuery(getActivity(), null);
+                updateItems();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateItems(){
+        String query = QueryPreferences.getStoredQuery(getActivity());
+        new FetchItemsTask(query).execute();
     }
 
     @Override
