cd330f7a7da2e64917d9ebbc3f1a1ecb2ecfe97c
palance <palanceli@163.com>
2017-11-18 13:26:16 +0800

    commit changes

--- PhotoGallery/app/src/main/java/com/bnrg/photogallery/PollService.java
+++ PhotoGallery/app/src/main/java/com/bnrg/photogallery/PollService.java
@@ -3,10 +3,13 @@ package com.bnrg.photogallery;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -22,7 +25,7 @@ import java.util.concurrent.TimeUnit;
 
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
-    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+    private static final long POLL_INTERVAL_MS = TimeUnit.SECONDS.toMillis(10);
 
     public static Intent newIntent(Context context){
         return new Intent(context, PollService.class);
@@ -74,18 +77,35 @@ public class PollService extends IntentService {
             Resources resources = getResources();
             Intent i = PhotoGalleryActivity.newIntent(this);
             PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+            NotificationManager notificationManager =
+                    (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+            String notificationChannelId = "PhotoGalleryChannel";
+            CharSequence channelName = "PGChannelName";
+            String channelDescription = "PGChannelDescription";
+            int channelImportance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    notificationChannelId, channelName, channelImportance);
+            notificationChannel.setDescription(channelDescription);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(notificationChannel);
+
             Notification notification = new
-                    NotificationCompat.Builder(this)
+                    Notification.Builder(this)
                     .setTicker(resources.getString(R.string.new_pictures_title))
                     .setSmallIcon(android.R.drawable.ic_menu_report_image)
                     .setContentTitle(resources.getString(R.string.new_pictures_title))
                     .setContentText(resources.getString(R.string.new_pictures_text))
                     .setContentIntent(pi)
                     .setAutoCancel(true)
+                    .setChannelId(notificationChannelId)
                     .build();
-
-            NotificationManagerCompat notificationManager =
-                                    NotificationManagerCompat.from(this);
+//
+//            NotificationManagerCompat notificationManager =
+//                                    NotificationManagerCompat.from(this);
             notificationManager.notify(0, notification);
         }
         QueryPreferences.setLastResultId(this, resultId);
