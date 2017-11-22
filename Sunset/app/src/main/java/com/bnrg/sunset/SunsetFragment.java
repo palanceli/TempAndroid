cb601555fdb10aab2a9d17f703bf85f496a64952
palance <palanceli@163.com>
2017-11-22 23:53:26 +0800

    commit changes

--- Sunset/app/src/main/java/com/bnrg/sunset/SunsetFragment.java
+++ Sunset/app/src/main/java/com/bnrg/sunset/SunsetFragment.java
@@ -1,16 +1,22 @@
 package com.bnrg.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 
 /**
  * Created by palance on 2017/11/22.
  */
 
 public class SunsetFragment extends Fragment {
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
     public static SunsetFragment newInstance(){
         return new SunsetFragment();
     }
@@ -20,6 +26,26 @@ public class SunsetFragment extends Fragment {
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_sunset, container,
                 false);
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startAnimation();
+            }
+        });
         return view;
+    }
+
+    private void startAnimation(){
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+        ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(
+                mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+        heightAnimator.setInterpolator(new AccelerateInterpolator());
+        heightAnimator.start();
     }
 }
